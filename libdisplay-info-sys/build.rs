fea41f53f792eead201f460e2b3751f545475542
may <m4rch3n1ng@gmail.com>
2025-05-15 16:41:06 +0200

    replace system-deps with plain pkg-config
    the system-deps dependency was way overkill for finding the version
    of a single dependency and was unnecessarily pulling in the entirety of
    toml-edit.

--- libdisplay-info-sys/build.rs
+++ libdisplay-info-sys/build.rs
@@ -1,18 +1,18 @@
-use system_deps::Dependencies;
-
 fn main() {
     if std::env::var("DOCS_RS").is_ok() {
         // don't link against unavailable native lib in doc.rs builds
         return;
     }
 
-    let deps = system_deps::Config::new().probe().unwrap();
-    auto_detect(&deps);
+    auto_detect();
 }
 
 #[cfg(feature = "auto")]
-fn auto_detect(deps: &Dependencies) {
-    let native_lib = deps.get_by_name("libdisplay-info").unwrap();
+fn auto_detect() {
+    let native_lib = pkg_config::Config::new()
+        .range_version("0.1.0".."0.4.0")
+        .probe("libdisplay-info")
+        .unwrap();
     let native_version = semver::Version::parse(&native_lib.version).unwrap();
     let is_v3 = semver::VersionReq::parse(">=0.3")
         .unwrap()
@@ -34,4 +34,4 @@ fn auto_detect(deps: &Dependencies) {
 }
 
 #[cfg(not(feature = "auto"))]
-fn auto_detect(_: &Dependencies) {}
+fn auto_detect() {}
